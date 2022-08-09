5dbefe90a6a5a7770f8690bd0277ad20b87b98b1
Vishal Singh <mevishal.vs04@gmail.com>
2022-08-09 21:15:01 +0530

    files added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [temperature,settemperature] = useState(0)
+  const [temperaturecolor,settemperaturecolor] = useState('cold')
+  const increment = ()=>{
+    const newtemp=temperature+1
+    if (newtemp>=15){
+      settemperaturecolor('hot')
+    }
+    else if(newtemp<15 && newtemp>=10){
+      settemperaturecolor('neutral')
+    }else if(newtemp<10 && newtemp>=0){
+      settemperaturecolor('cold')
+    }else if(newtemp<0){
+      settemperaturecolor('freezing')
+    }
+    settemperature(newtemp)
+    
+  }
+  const decrement = ()=>{
+    const newtemp=temperature-1
+    if (newtemp>=15){
+      settemperaturecolor('hot')
+    }
+    
+    else if(newtemp<15 && newtemp>=10){
+      settemperaturecolor('neutral')
+    }else if(newtemp<10 && newtemp>=0){
+      settemperaturecolor('cold')
+    }else if(newtemp<0){
+      settemperaturecolor('freezing')
+    }
+    settemperature(temperature-1)
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <>
+    <h1>
+      Color Changer according to temperature using REACT
+    </h1>
+  
+    <div className="app-container" >
+    <div className="temperature-display-container">
+          <div className={`temperature-display ${temperaturecolor}`} id="app">
+            {temperature}°C
+
+      </div>
     </div>
+    <div className="button-container">
+    <button id="increment" onClick={increment}>+</button>
+    <button id="decrement" onClick={decrement}>-</button>
+
+    </div>
+
+    </div>
+  
+  </>
   );
 }
 
